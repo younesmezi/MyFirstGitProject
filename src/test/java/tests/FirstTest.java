a83bacc90c59e317834aaaeb9bc0c571745bf0ad
younesmezi <younes2472002@hotmail.com>
2019-07-30 21:52:32 -0400

    update

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -1,8 +1,9 @@
 package tests;
 
-public class FirstTest {
+public  class FirstTest {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("test");
+        System.out.println("test2");
     }
 }
